e27fbab891f4699ee406398bd9613fbeed525707
teach <morus95@mail.ru>
2023-03-14 17:14:30 +0300

    first commit

---
+++ src/main/java/com/calculator/rmplcalculator/RmplCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.calculator.rmplcalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RmplCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RmplCalculatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/calculator/rmplcalculator/RmplCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.calculator.rmplcalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RmplCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
