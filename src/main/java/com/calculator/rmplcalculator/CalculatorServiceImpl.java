b777a1931190a35ff678cb06e8fbb57720d2d3f9
teach <morus95@mail.ru>
2023-03-15 14:11:36 +0300

    second commit

---
+++ src/main/java/com/calculator/rmplcalculator/CalculatorServiceImpl.java
@@ -0,0 +1,30 @@
+package com.calculator.rmplcalculator;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CalculatorServiceImpl implements CalculatorService {
+    public String hello() {
+        return "Добро пожаловать в калькулятор";
+    }
+
+    public String plus(int num1, int num2) {
+        return num1 + " + " + num2 + " = " + (num1 + num2);
+    }
+
+    public String minus(int num1, int num2) {
+        return num1 + " - " + num2 + " = " + (num1 - num2);
+    }
+
+    public String divide(int num1, int num2) {
+        if (num2 == 0) {
+            return num1 + " / " + num2 + " - " + "Деление невозможно";
+        } else {
+            return num1 + " / " + num2 + " = " + num1 / (double) num2;
+        }
+    }
+
+    public String multiply(int num1, int num2) {
+        return num1 + " * " + num2 + " = " + (num1 * num2);
+    }
+}
